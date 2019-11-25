0b693dd2455de8bd71d12057015f7dfab863a256
edwm101 <rkingedwm101@users.noreply.github.com>
2019-11-25 19:10:32 +0100

    Fix

--- api/v2/controllers/public/products.php
+++ api/v2/controllers/public/products.php
@@ -4,7 +4,7 @@ $router->get('all', function () {
     global  $db, $result, $_get;
     $max = $_get->max;
     try {
-        $result["items"] = $db->select('*')
+        $result["items"] = $db->select('p.id,p.name,p.price,p.image,c.name as cat_name')
             ->from("products p")
             ->join("left join categories c on c.id = p.category_id ")
             ->orderBy("p.id desc")
@@ -19,11 +19,33 @@ $router->get('', function () {
     global  $db, $result, $_get;
     $id = $_get->id;
     try {
-         $result["info"] = $db->select('*')
+        $info = $db->select('*')
             ->from("products p")
-            ->join("left join categories c on c.id = p.category_id ")
             ->where("p.id=?", $id)
             ->execute()->fetchObject();
+
+        $info->images =   explode(",", str_replace('small_default', 'large_default', $info->images));
+        $result["info"] = $info;
+    } catch (Exception $e) {
+        Func::error("PDO_EXCEPTION", 400, $e->getMessage());
+    };
+});
+
+
+$router->get('find', function () {
+    global  $db, $result, $_get;
+
+    $q = $_get->q;
+    $q = str_replace("+", " ", $q);
+
+    try {
+        $items = $db->select('*')
+            ->from("products")
+            ->where("LOWER(name) LIKE LOWER(concat('%',?, '%'))  ", $q)
+            ->orderBy("LENGTH(name) asc")
+            ->limit("20")->execute()->fetchAll();
+        $result["query"] = ucfirst($q);
+        $result["items"] = $items;
     } catch (Exception $e) {
         Func::error("PDO_EXCEPTION", 400, $e->getMessage());
     };
--- sy/src/Controller/AppController.php
+++ sy/src/Controller/AppController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use App\Entity\Products;
 use App\Form\Products1Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -16,6 +16,8 @@ use Symfony\Component\HttpClient\HttpClient;
  */
 class AppController extends AbstractController
 {
+    const API_URL = "http://localhost/go4prod/api/v2/public/";
+
     /**
      * @Route("/", name="products_index", methods={"GET"})
      */
@@ -23,13 +25,40 @@ class AppController extends AbstractController
     {
 
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'https://api.github.com/repos/symfony/symfony-docs');
-        $products = $this->getDoctrine()
-            ->getRepository(Products::class)
-            ->findBy([], ['id' => 'DESC'], 14);
-
+        $response = $httpClient->request('GET', self::API_URL . 'products/all?max=10');
+        $products = $response->toArray();
+        $products = $products["items"];
         return $this->render('index.html.twig', [
             'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/shop/", name="shop_show", methods={"GET"})
+     */
+    public function shop(Request $request): Response
+    {
+        $q = $request->query->get('q');
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', self::API_URL . 'products/find/?q='.$q);
+        $products = $response->toArray();
+        $products = $products;
+        return $this->render('shop.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/products/{id}", name="products_show", methods={"GET"})
+     */
+    public function show($id): Response
+    {
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', self::API_URL . 'products?id=' . $id);
+        $product = $response->toArray();
+        $product = $product["info"];
+        return $this->render('show.html.twig', [
+            'product' => $product,
         ]);
     }
     /**
@@ -38,21 +67,10 @@ class AppController extends AbstractController
     public function account_index(): Response
     {
 
-        $products = $this->getDoctrine()
-            ->getRepository(Products::class)
-            ->findBy([], ['id' => 'DESC'], 14);
+
 
         return $this->render('users/account.html.twig', [
-            'products' => $products,
-        ]);
-    }
-    /**
-     * @Route("/products/{id}", name="products_show", methods={"GET"})
-     */
-    public function show(Products $product): Response
-    {
-        return $this->render('products/show.html.twig', [
-            'product' => $product,
+            'products' => "",
         ]);
     }
 }
